3aaff3f1473341dee6b6870cf597d558d8596f64
omarusmani <omarusmani3@hotmail.com>
2022-07-03 15:18:44 -0500

    got users

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
+const bodyParser = require("body-parser");
+const usersRouter = require('./routes/users');
+const users =require("./data/index");
+
 const port = process.env.PORT || 4000
 
+app.use(bodyParser.json())
+// app.use('/users', usersRouter)
+app.get("/users", (req,res)=>{
+  res.json(users);
+})
 app.get('/', (req, res) => res.send('default route'))
-
 app.listen(port, () => {
   console.log('app is listening on:', port)
 })
---
+++ routes/users.js
@@ -0,0 +1,15 @@
+// const express = require('express')
+// const usersController = require('../controller/users')
+// const router = express.Router()
+
+// router.get('/', usersController.getAllUsers)
+
+// router.get('/:id', usersController.getUserById)
+
+// router.post('/', usersController.createUser)
+
+// router.put('/:id', usersController.updateUserById)
+
+// router.delete('/:id', usersController.deleteUserById)
+
+// module.exports = router
